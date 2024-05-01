95a9f87a87e5cbc50f1397d6f9b3ee05e491c1d2
ridwaanhall <erbyl14@gmail.com>
2024-05-01 17:20:57 +0700

    5.0/intro/tutorial07

--- polls/admin.py
+++ polls/admin.py
@@ -1,16 +1,25 @@
 from django.contrib import admin
 
-from .models import Question, Choice
+from .models import Choice, Question
+
+
+class ChoiceInline(admin.TabularInline): # before: StackedInline
+    model = Choice
+    extra = 3
 
 
 class QuestionAdmin(admin.ModelAdmin):
-    # fields = ["pub_date", "question_text"]
     fieldsets = [
         (None, {"fields": ["question_text"]}),
-        ("Date information", {"fields": ["pub_date"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
     ]
+    inlines = [ChoiceInline]
+    # list_display = ["question_text", "pub_date"]
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+    # adds a “Filter” sidebar that lets people filter the change list by the pub_date field
+    list_filter = ["pub_date"]
+    #adds a search box at the top of the change list
+    search_fields = ["question_text"]
 
 
 admin.site.register(Question, QuestionAdmin)
-
-admin.site.register(Choice)
